5f18894bd1fcb4d46a460f8cbefa06fbf6d7a46f
g74253 <38565604+g74253@users.noreply.github.com>
2020-06-05 15:54:56 -0600

    Vspointer base
    Vspointer default, preparado para incorporacion.

--- Vs_Code/main.cpp
+++ Vs_Code/main.cpp
@@ -5,8 +5,20 @@ using namespace std;
 int main() {
     Vsptr<int> myPtr = Vsptr<int>::New();
     Vsptr<int> myPtr2 = Vsptr<int>::New();
+    Vsptr<int> myPtr3 = Vsptr<int>::New();
     *myPtr = 47;
+    myPtr3 = 32;
+    myPtr2 = myPtr;
+    int valor = &myPtr;
+    std ::cout << "El valor de myPtr(*myPtr = 47;):" <<std ::endl;
     std ::cout << &myPtr <<std ::endl;
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "El valor es ( int valor = &myPtr):" << std::endl;
+    std::cout << valor << std::endl;
+    std ::cout << "El valor de myPtr3(myPtr3 = 32;):" <<std ::endl;
+    std::cout << &myPtr3 << std::endl;
+    std ::cout << "El valor de myPtr2(myPtr2 = myPtr;):" <<std ::endl;
+    std::cout << &myPtr2<< std::endl;
+
+
     return 0;
 }
